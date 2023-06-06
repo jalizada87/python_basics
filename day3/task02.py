c87447cd5de7976cbbca595882627da167a85693
Javid Alizada <javid.alizada@taotesting.com>
2023-06-06 18:21:43 +0400

    Upgrade task02 in day3 and add filename and size

--- day3/task02.py
+++ day3/task02.py
@@ -3,5 +3,11 @@ import os
 current_dir = os.getcwd()
 print(current_dir)
 
-file_size = os.path.getsize(current_dir)
-print(file_size)
+list_directory = os.listdir(current_dir)
+print(' '.join(list_directory))
+
+
+for i in list_directory:
+    file_size = os.path.getsize(i)
+    file_name = os.path.basename(i)
+    print(f"name: {file_name}, size: {file_size} byte")
